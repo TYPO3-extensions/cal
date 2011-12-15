71fbee99af2d6bb1b70dd8dd587f2546debbcc86
mario_matzulla <mario_matzulla@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-15 20:47:20 +0000

    added compatibility for 4.6 (Bug #32258)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cal/trunk@55547 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '3.8.0-4.5.99',
+			'typo3' => '3.8.0-4.6.99',
 		),
 		'conflicts' => array(
 		),

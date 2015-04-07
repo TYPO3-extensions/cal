36482332c404478976f2c116e031714432bd159d
Mario Matzulla <mario@matzullas.de>
2015-04-07 20:05:12 +0200

    fixed locallang reference

--- controller/class.tx_cal_wizicon.php
+++ controller/class.tx_cal_wizicon.php
@@ -49,7 +49,7 @@ class tx_cal_wizicon {
 		return $wizardItems;
 	}
 	function includeLocalLang() {
-		$llFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath ('cal') . 'Resources/Private/Language/locallang.xml';
+		$llFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath ('cal') . 'Resources/Private/Language/locallang_plugin.xml';
 		if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger (TYPO3_version) >= 4006000) {
 			$localizationParser = new \TYPO3\CMS\Core\Localization\Parser\LocallangXmlParser();
 			$LOCAL_LANG = $localizationParser->getParsedData ($llFile, $GLOBALS ['LANG']->lang);

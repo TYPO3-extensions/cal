a308b2b7688938482eb879fc1fa3ca317d8ce776
Mario Matzulla <mario@matzullas.de>
2015-10-04 21:01:44 +0200

    Fixed Bug #70322: FormEngine removed in CMS7.5 breaks date processing

--- Classes/Slot/FormDataPreprocessorSlot.php
+++ Classes/Slot/FormDataPreprocessorSlot.php
@@ -14,11 +14,31 @@ namespace TYPO3\CMS\Cal\Slot;
  */
 
 use TYPO3\CMS\Backend\Form\DataPreprocessor;
+use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Slot class for the FormEngine DataPreprocessor
  */
-class FormDataPreprocessorSlot {
+class FormDataPreprocessorSlot implements FormDataProviderInterface {
+	
+	public static function register() {
+		if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 7005000) {
+			$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\TYPO3\CMS\Cal\Slot\FormDataPreprocessorSlot::class] = array(
+					'before' => array(
+							\TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowDateTimeFields::class,
+					),
+			);
+		} else {
+			\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
+					\TYPO3\CMS\Backend\Form\DataPreprocessor::class,
+					'fetchRecordPostProcessing',
+					\TYPO3\CMS\Cal\Slot\FormDataPreprocessorSlot::class,
+					'fetchCalRecordPostProcessing'
+			);
+		}
+	}
 
 	/**
 	 * Fetch the tx_cal_* records and manipulate them
@@ -26,7 +46,7 @@ class FormDataPreprocessorSlot {
 	 * @param DataPreprocessor $recordData
 	 * @return void
 	 */
-	function fetchCalRecordPostProcessing(DataPreprocessor $recordData) {
+	public function fetchCalRecordPostProcessing(DataPreprocessor $recordData) {
 
 		if (preg_match('/^tx_cal_(.*)$/', key($recordData->regTableItems)) == FALSE) {
 			return;
@@ -40,5 +60,34 @@ class FormDataPreprocessorSlot {
 
 			$recordData->regTableItems_data[$key] = $value;
 		}
+	}
+	
+	/**
+	 * Migrate date and datetime db field values to timestamp
+	 *
+	 * @param array $result
+	 * @return array
+	 */
+	public function addData(array $result) {
+		$dateTimeFormats = $this->getDatabase()->getDateTimeFormats($result['tableName']);
+		foreach ($result['vanillaTableTca']['columns'] as $column => $columnConfig) {
+			if (isset($columnConfig['config']['tx_cal_event'])) {
+				
+				
+				$mainFields = new \TYPO3\CMS\Cal\Hooks\TceFormsGetmainfields();
+				$mainFields->getMainFields_preProcess($result['tableName'], $result['databaseRow'], NULL);
+				
+				return $result;
+				
+			}
+		}
+		return $result;
+	}
+	
+	/**
+	 * @return DatabaseConnection
+	 */
+	protected function getDatabase() {
+		return $GLOBALS['TYPO3_DB'];
 	}
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -857,13 +857,8 @@ $confArr = unserialize ($GLOBALS ['TYPO3_CONF_VARS'] ['EXT'] ['extConf'] ['cal']
 $GLOBALS ['TYPO3_CONF_VARS'] ['SC_OPTIONS'] ['t3lib/class.t3lib_tcemain.php'] ['processDatamapClass'] ['tx_cal'] = 'TYPO3\\CMS\\Cal\\Hooks\\TceMainProcessdatamap';
 $GLOBALS ['TYPO3_CONF_VARS'] ['SC_OPTIONS'] ['t3lib/class.t3lib_tcemain.php'] ['processCmdmapClass'] ['tx_cal'] = 'TYPO3\\CMS\\Cal\\Hooks\\TceMainProcesscmdmap';
 
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= '7001000') {
-	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
-		\TYPO3\CMS\Backend\Form\DataPreprocessor::class,
-		'fetchRecordPostProcessing',
-		\TYPO3\CMS\Cal\Slot\FormDataPreprocessorSlot::class,
-		'fetchCalRecordPostProcessing'
-	);
+if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 7001000) {
+	\TYPO3\CMS\Cal\Slot\FormDataPreprocessorSlot::register();
 } else {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass']['tx_cal'] = 'TYPO3\\CMS\\Cal\\Hooks\\TceFormsGetmainfields';
 }
